73e5f1ea5ed5bef2cf47d4be3dd42b3d416c5f33
fabri <fguespe@gmail.com>
2023-09-23 22:12:20 -0400

    font fix

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import "./polyfills";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import InboxPage from "./InboxPage-hooks";
+import "./index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
